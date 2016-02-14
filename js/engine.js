2a62b8029b33e0615ae6ff7c24fa6ae9aa8b1534
Spodar <unknonimus@gmail.com>
2016-02-14 19:10:24 +0200

    example

---
+++ js/engine.js
@@ -0,0 +1,23 @@
+﻿jQuery(document).ready(function() {
+
+// BEGIN of script for toggle items on product page
+	$('.btn-read').click(function(){
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$(this).html('Read More');
+			$(this).parents('.featured-item').find('.featured-item-detail').slideUp();
+		}
+		else {
+			$('.featured-item-detail').slideUp();
+			$('.btn-read').removeClass('active');
+			$('.btn-read').html('Read More');
+			$(this).parents('.featured-item').find('.featured-item-detail').slideDown();
+			$(this).toggleClass('active')
+			$(this).html('Read Less');
+		}
+	});
+// END of script for toggle items on product page
+
+
+
+});
